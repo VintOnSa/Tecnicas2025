6ab2a0d6fb76779e225b6f2eb081d521c04741c3
Vint <127328649+VintOnSa@users.noreply.github.com>
2025-03-31 14:18:53 -0300

    Update settings.py

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
